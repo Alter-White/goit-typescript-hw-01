35ab6574b45ea527621b58659d75666d7ca3b3d8
Ivan <ivanprime96@gmail.com>
2024-05-31 17:39:00 +0300

    Update 3.ts

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -2,7 +2,7 @@
 // const objB = { age: 25 };
 
 function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
-  return { ...objA, ...objB };
+  return Object.assign(objA, objB);
 }
 
 // const mergedObj = merge(objA, objB);
